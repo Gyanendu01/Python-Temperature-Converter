6e0a35ca71c45760399d9012c59e911e918628c4
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-26 09:21:23 +0530

    Add description and main.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description:
+
+This program is responsible to convert a given temperature in degrees
+Fahrenheit to degrees Celsius and degrees Kelvin. This program will round all conversions to a
+precision of four decimal places. Lastly, This program will display the results in a convenient
+table style format.
---
+++ main.py
@@ -0,0 +1,19 @@
+print("\n\tWELCOME TO THE TEMPERATURE CONVERSION APPLICATION")
+
+def usr_input():
+    # Get the user-name and temperature value
+    usr_name = input("\n\tEnter your username: ")
+    temp = float(input("\n\tWhat is the given temperature in degrees Fahrenheit: "))
+    return usr_name, temp
+
+def temp_conversion(name,temp):
+    print("\n\tHello {}! Temperature Conversion table is as below".format(name))
+    
+    # LOGIC FOR temperature conversion
+    print("\n\tDegrees Fahrenheit:\t\t\t {}".format(temp))
+    print("\tDegrees Celcius:\t\t\t {}".format((temp-32)*5/9))
+    print("\tDegrees Kelvin:\t\t\t\t {}".format(((temp-32)*5/9)+273.15))
+
+# MAIN PROGRAM
+usr_data = usr_input()
+temp_conversion = temp_conversion(usr_data[0],usr_data[1])
